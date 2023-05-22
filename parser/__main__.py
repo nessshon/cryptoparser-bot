0d5bd9ab8c4451101b9598cd6ccab6de6ee9761a
nessshon <shonness98@gmail.com>
2023-05-22 23:13:00 +0500

    Added disabling parsing at night from 23:00 to 8:00

--- parser/__main__.py
+++ parser/__main__.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
 import random
+import datetime
 
+import pytz
 from aiogram import Bot
 
-from app.config import load_config
+from app.config import load_config, TIME_ZONE
 from app.db.sqlite.manage import Database
 from parser.main import parse
 
@@ -25,10 +27,15 @@ async def main():
     logging.info("Bot initialized!")
 
     while True:
+        now = datetime.datetime.now(tz=pytz.timezone(TIME_ZONE)).time()
+        if now >= datetime.time(23, 0) or now < datetime.time(8, 0):
+            logging.info("Skipping parsing...")
+            await asyncio.sleep(600)
+            continue
         logging.info("Parsing...")
         await parse(db, bot, config)
         logging.info("Parsing finished!")
-        await asyncio.sleep(random.randint(3400, 3800))
+        await asyncio.sleep(random.randint(3000, 4200))
 
 
 if __name__ == '__main__':
