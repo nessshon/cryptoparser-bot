ff794bf5f958c6828c83f3f2daf4af3a05a267e5
nessshon <shonness98@gmail.com>
2023-05-30 15:24:05 +0500

    updated

--- parser/driver.py
+++ parser/driver.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_driver() -> WebDriver:
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     return driver
