730550a4d7d8b977530066eb2c3965480b5844a5
nessshon <shonness98@gmail.com>
2023-10-11 20:23:21 +0500

    updated

--- app/parser/driver.py
+++ app/parser/driver.py
@@ -9,6 +9,8 @@ from webdriver_manager.core.utils import read_version_from_cmd
 
 def get_driver() -> WebDriver:
     chrome_options = Options()
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"  # noqa
+    chrome_options.add_argument(f"user-agent={user_agent}")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
